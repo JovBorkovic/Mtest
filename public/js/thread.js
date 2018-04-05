7be00a8a6a5eb5f6722a57f889cb72b0277e93bb
JovBorkovic <jovan-borkovic@hotmail.com>
2018-04-05 18:25:31 -0400

    changed on the thread page

---
+++ public/js/thread.js
@@ -0,0 +1,46 @@
+$().ready(function(){
+    
+    
+    $.fn.isOnScreen = function(){
+    
+        var win = $(window);
+
+        var viewport = {
+            top : win.scrollTop(),
+            left : win.scrollLeft()
+        };
+        viewport.right = viewport.left + win.width();
+        viewport.bottom = viewport.top + win.height();
+
+        var bounds = this.offset();
+        bounds.right = bounds.left + this.outerWidth();
+        bounds.bottom = bounds.top + this.outerHeight();
+
+        return (!(viewport.right < bounds.left || viewport.left > bounds.right || viewport.bottom < bounds.top || viewport.top > bounds.bottom));
+
+    };
+
+    var T_Container = $('#T_Container');
+    
+    
+    $(window).scroll(function () {
+        
+        if($('.T_Container').length > 0) {
+            if ($('.T_Container').isOnScreen() === false){
+                T_Container.addClass('threadNot');
+                T_Container.removeClass('T_Container');
+            }
+        }
+        if($('.threadNot').length > 0) {
+            if ($(window).scrollTop() === 0){
+                T_Container.removeClass('threadNot');
+                T_Container.addClass('T_Container');
+            }
+        }
+
+    });
+    
+    
+    
+    
+});
