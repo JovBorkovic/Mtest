56714936ab01c4564c03bf71304618d505e8c17e
JovBorkovic <jovan-borkovic@hotmail.com>
2018-04-05 22:10:00 -0400

    a

--- public/js/thread.js
+++ public/js/thread.js
@@ -24,18 +24,24 @@ $().ready(function(){
     
     
     $(window).scroll(function () {
-        
-        if($('.T_Container').length > 0) {
-            if ($('.T_Container').isOnScreen() === false){
-                T_Container.addClass('threadNot');
-                T_Container.removeClass('T_Container');
+        if ($(window).width() > 1024){
+            if($('.T_Container').length > 0) {
+                if ($('.T_Container').isOnScreen() === false){
+                    T_Container.addClass('threadNot');
+                    T_Container.removeClass('T_Container');
+                }
+            }
+            if($('.threadNot').length > 0) {
+                if ($(window).scrollTop() <= 64){
+                    T_Container.removeClass('threadNot');
+                    T_Container.addClass('T_Container');
+                }
             }
         }
-        if($('.threadNot').length > 0) {
-            if ($(window).scrollTop() === 0){
-                T_Container.removeClass('threadNot');
-                T_Container.addClass('T_Container');
-            }
+        else {
+            T_Container.removeClass('T_Container');
+            T_Container.removeClass('threadNot');
+            T_Container.addClass('T_Container');
         }
 
     });
